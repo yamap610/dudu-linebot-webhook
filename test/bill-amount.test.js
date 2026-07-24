a155158290f5a7edc22fe80712313600755cd6db
Yui <yamap610@gmail.com>
2026-07-24 18:54:26 +0800

    Simplify bill status display

--- test/bill-amount.test.js
+++ test/bill-amount.test.js
@@ -4,15 +4,26 @@ const bot = require('../lib/bot');
 
 test('繳費選單可選擇帳單並預填更新金額指令', () => {
   const item = {
-    id: 'a90447cd77218293b11b81d4fda86c3d', name: '台灣大哥大', price: 999,
+    id: 'a90447cd77218293b11b81d4fda86c3d', name: '電信費_台灣大哥大', price: 999,
     dueDate: '2026-07-20', amountUpdatedDate: '2026-07-18', paused: false,
   };
   const billsJson = JSON.stringify(bot.billsMessage([item]));
   assert.match(billsJson, /update_amount_menu/);
   assert.match(billsJson, /\$999（07\/18更新）/);
+  assert.match(billsJson, /"label":"繳費"/);
   const menuJson = JSON.stringify(bot.updateAmountMenuMessage([item]));
-  assert.match(menuJson, /更新金額 台灣大哥大 /);
+  assert.match(menuJson, /更新金額 電信費_台灣大哥大 /);
   assert.match(menuJson, /openKeyboard/);
+});
+
+test('固定金額不顯示更新日，未來到期日不顯示年份', () => {
+  const json = JSON.stringify(bot.billsMessage([{
+    id: 'a90447cd77218293b11b81d4fda86c3d', name: 'Netflix', price: 390,
+    dueDate: '2026-10-05', amountUpdatedDate: '2026-07-18', paused: false,
+  }]));
+  assert.match(json, /📅 10\/05 \$390/);
+  assert.doesNotMatch(json, /07\/18更新/);
+  assert.doesNotMatch(json, /2026\/10\/05/);
 });
 
 test('更新金額會修改 Notion 價格並回覆更新日期', async () => {
